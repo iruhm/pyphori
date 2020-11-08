05aa4f7979b7eb375165b9360080333594a2a432
ingo <ingo@ws01-ub1804lts>
2020-11-08 08:51:06 +0100

    implemented initial argument parser

--- pyphori.py
+++ pyphori.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
+import argparse
+
+def main():
+    print("\nPython Photo Rename and Indexing Script\n")
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-d",
+                        "--data_dir",
+                        type = str,
+                        help = "data directory that contains the photos",
+                        required = True)
+    
+    
+    args = parser.parse_args()
 
 
 #=============
 # main program
 #=============
-
-print("\nPython Photo Rename and Indexing Script\n")
+if __name__ == "__main__":
+    main()
